d385b68d27013a82308b19a446d458a31d38de0e
WasinPanitan <wasin.p1995@gmail.com>
2023-09-26 17:07:21 +0700

    add dogModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { CatsService } from './cats/cats.service';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, DogsModule],
   controllers: [AppController],
   providers: [AppService, CatsService],
 })
