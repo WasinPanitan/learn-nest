a572de8802b5ce6c3df6e9d34d7726f68ebdfa84
WasinPanitan <wasin.p1995@gmail.com>
2023-09-29 14:12:07 +0700

    reports and users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { CatsService } from './cats/cats.service';
 import { DogsModule } from './dogs/dogs.module';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [CatsModule, DogsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest_dev',
+      entities: [],
+      synchronize: true,
+    }),
+    CatsModule,
+    DogsModule,
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, CatsService],
 })
