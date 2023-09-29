c79fb4ead752efbf50ddc472a7a999070080962f
WasinPanitan <wasin.p1995@gmail.com>
2023-09-29 15:18:24 +0700

    connect user service to controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     console.log(body);
+    this.userService.create(body.email, body.password);
   }
 }
